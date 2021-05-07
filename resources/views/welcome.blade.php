aaeb496689fef971afad32b0e133b402a8e906a9
rapaks0428 <fiebrich.1b12@op.pl>
2021-05-07 13:55:23 +0000

    Zmiana pliku welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ESSUNIA Świrki
                 </div>
 
                 <div class="links">
